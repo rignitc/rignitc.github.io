a9b7b9e8aa252adae3bc0313d55653fedc910d81
SlyPredator <SlyPredator@users.noreply.github.com>
2025-12-31 19:31:34 +0000

    Remove preview for PR 7 🛬

--- pr-preview/pr-7/src/scripts/config.js
+++
@@ -1,4 +0,0 @@
-// This file is auto-generated. Do not edit manually.
-window.RIGNITC_CONFIG = {
-    GITHUB_TOKEN: "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"
-};
